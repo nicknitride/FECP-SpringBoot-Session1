4dce9bdceca2334e7578925e978e7df3bf9cd38e
Nicholo Patrick Pardines <nick123abl@gmail.com>
2025-07-11 12:31:26 +0800

    Added animal and species

---
+++ src/main/java/org/ZooFloresManaloPardines/Bird.java
@@ -0,0 +1,8 @@
+package org.ZooFloresManaloPardines;
+
+public abstract class Bird extends Animal{
+    public Building location;
+    public void roam(){
+        System.out.println("Bird roam.");
+    }
+}
---
+++ src/main/java/org/ZooFloresManaloPardines/Pachyderm.java
@@ -0,0 +1,8 @@
+package org.ZooFloresManaloPardines;
+
+public abstract class Pachyderm extends Animal{
+    public Building location;
+    public void roam(){
+        System.out.println("Pachyderm roam.");
+    }
+}
