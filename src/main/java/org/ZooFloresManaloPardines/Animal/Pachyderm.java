e52f2a11d2a05bcaf2620aca8ff9679927f364ae
Nicholo Pardines <nick123abl@gmail.com>
2025-07-13 11:38:16 +0800

    Added getters and setters within species

--- src/main/java/org/ZooFloresManaloPardines/Animal/Pachyderm.java
+++ src/main/java/org/ZooFloresManaloPardines/Animal/Pachyderm.java
@@ -7,4 +7,18 @@ public abstract class Pachyderm extends Animal {
     public void roam(){
         System.out.println("Pachyderm roam.");
     }
+    public Building getLocation(){
+        System.out.println("Print <Location> here once method in Building is defined");
+        return location;
+    }
+    public String getName(){
+        System.out.printf("Animal's name is %s",this.name);
+        return this.name;
+    }
+    public boolean getHealth(){
+        return this.isHealthy;
+    }
+    public void setHealth(boolean healthValue){
+        this.isHealthy = healthValue;
+    }
 }
